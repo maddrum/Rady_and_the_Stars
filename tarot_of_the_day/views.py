9f67ba11f7ad53e1b403771498ee367fe1dee90f
maddrum <maddrum9@gmail.com>
2018-05-08 21:16:48 +0300

    Tarot of the day initial

--- tarot_of_the_day/views.py
+++ tarot_of_the_day/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from tarot_of_the_day import models
-
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, CreateView
 from django.utils import timezone
 
 
-# Create your views here.
+@login_required
 def card_pick(request):
     current_date = timezone.localdate()
     data_dict = {}
@@ -14,9 +14,11 @@ def card_pick(request):
     if day_check.__len__() == 0:
         models.UserCard().random_card(logged_user_id=user_id)
     day_tarot = models.TarotCard.objects.select_related().filter(
-        user_card__date_used=current_date)
+        user_card__date_used=current_date, user_card__username_id=user_id)
     print(day_tarot)
     data_dict['day_tarot'] = day_tarot
+    user_tarot = models.UserCard.objects.filter(date_used=current_date, username_id=user_id)
+    data_dict['user_tarot'] = user_tarot
     return render(request, 'tarot_of_the_day/day_card.html', data_dict)
 
 
